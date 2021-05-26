f6b32e06532a4a4343903587f471bf46bf8375ce
Manuelg <manugm_00@hotmail.com>
2021-05-26 18:34:39 -0300

    tabla de informacion

--- pages/index.js
+++ pages/index.js
@@ -16,10 +16,35 @@ const Index = () => {
               Aqui esta el mapa
             </p>
           </div>
-          <div className="info">
+          <div className="info">      
             <p className="description">
               Aqui esta la info
             </p>
+            <div className="Tabla">
+              <table border="2">
+                <caption>
+                  <tr><tr>Fecha inicio:</tr><tr>0</tr></tr>
+                  <tr>Fecha fin: </tr><tr>0</tr> 
+                </caption>
+                <thead>
+                    <th colSpan="3">
+                      Composicion de los eventos sonoros en el sector
+                    </th>
+                </thead>
+                <tr>
+                    <td>Fuentes de sonido</td><td> Porcentaje</td><td> Duracion </td>
+                </tr>
+                <tr>
+                    <td>Autos</td><td> % </td><td> 0 </td> 
+                </tr>
+                <tr>
+                    <td>Personas</td><td> % </td><td> 0 </td>
+                </tr>
+                <tr>
+                    <td>Animales</td><td> % </td><td> 0 </td>
+                </tr>
+              </table>
+            </div>
           </div>
         </div>
       </main>
